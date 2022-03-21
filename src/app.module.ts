3d812e61dcd76f5248dbff18a65d1e409f9625df
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-03-21 18:12:55 -0500

    Connect app engine with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { accessSecretVersion } from './config/secretmanager.config';
 import { LoggerMiddleware } from './middleware/logger';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '/cloudsql/pla-eafit:us-east1:pla-project',
+      port: 5432,
+      username: 'pla-admin',
+      password: 'pla-eafit',
+      database: 'pla-eafit',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
@@ -13,6 +26,5 @@ import { LoggerMiddleware } from './middleware/logger';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
     consumer.apply(LoggerMiddleware).forRoutes('*');
-    accessSecretVersion('projects/964659245205/secrets/DATABASE/versions/1');
   }
 }
