700a680b2cd8037e25910d77280bf1dbd0429152
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-03-21 21:53:04 -0500

    Make a connection between App engine and Cloudsql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middleware/logger';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'pla-admin',
+      password: 'admin',
+      entities: [User],
+      database: 'pla-eafit',
+      extra: {
+        socketPath: '/cloudsql/pla-eafit:us-east1:pla-project'
+      }
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  constructor(private connection: Connection){
+
+  }
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
