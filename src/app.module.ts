c93dc6e6e4b4bd8f61029ff63ad9dc5874001189
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-03-22 15:32:59 -0500

    Connect cloud with cloud sql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,31 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middleware/logger';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: '/cloudsql/pla-eafit:us-central1:pla-database',
+    username: 'pla-admin',
+    password: 'pla-eafit',
+    database: 'pla-project',
+    extra:{
+      sockethPath: '/cloudsql/pla-eafit:us-central1:pla-database'
+    },
+    entities: [User],
+    synchronize: true,
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
 
 export class AppModule implements NestModule {
+  constructor(private connection: Connection) {}
   configure(consumer: MiddlewareConsumer): void {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
