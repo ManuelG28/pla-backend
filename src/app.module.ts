1b0016428118a034a92e736efd2f77f6c2a14b84
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-03-22 15:27:17 -0500

    Set up ci/cd in cloudrun

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middleware/logger';
-import { User } from './user/user.entity';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      username: 'pla-admin',
-      password: 'admin',
-      entities: [User],
-      database: 'pla-eafit',
-      extra: {
-        socketPath: '/cloudsql/pla-eafit:us-east1:pla-project'
-      }
-    }),
-    UserModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
 
 export class AppModule implements NestModule {
-  constructor(private connection: Connection){
-
-  }
   configure(consumer: MiddlewareConsumer): void {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
